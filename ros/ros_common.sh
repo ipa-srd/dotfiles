2fdb6b0e1943f6e5c3e5cf04608023254f9db4f4
ipa-srd <Stefan.Doerr@ipa.fraunhofer.de>
2016-03-11 13:28:59 +0100

    adapted files with personal data

--- ros/ros_common.sh
+++ ros/ros_common.sh
@@ -1,6 +1,19 @@
+#source ~/git/msh_ws/devel/setup.bash
+#source ~/git/nav_ws/devel/setup.bash
+source ~/git/hmm_ws/devel/setup.bash
+
 #set ROS environment variables
-export ROS_EMAIL=mig@ipa.fhg.de
-export ROBOT=raw3-1
-#export ROBOT_ENV=ipa-kitchen
-#export ROBOT_ENV=ipa-4th-floor
+export ROS_EMAIL=srd@ipa.fhg.de
+
+# Robots
+export ROBOT=raw3-3
+#export ROBOT=cob4-2
+
+# Environments
 export ROBOT_ENV=ipa-apartment
+#export ROBOT_ENV=ipa-office
+#export ROBOT_ENV=saturn-ingolstadt
+
+# Master URI
+#export ROS_MASTER_URI=http://raw3-3-pc1:11311
+export ROS_MASTER_URI=http://localhost:11311
