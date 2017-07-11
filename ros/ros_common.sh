4d0f9ee95ec17b5e94521b939aa3526461214a42
ipa-srd <Stefan.Doerr@ipa.fraunhofer.de>
2017-07-11 14:32:29 +0200

    minor enhancements

--- ros/ros_common.sh
+++ ros/ros_common.sh
@@ -1,18 +1,27 @@
 #source ~/git/msh_ws/devel/setup.bash
+#source ~/git/baer_ws/devel/setup.bash
 #source ~/git/nav_ws/devel/setup.bash
 source ~/git/hmm_ws/devel/setup.bash
+#source ~/git/afkar_ws/devel/setup.bash
+#source ~/git/wisag_ws/devel/setup.bash
 
 #set ROS environment variables
 export ROS_EMAIL=srd@ipa.fhg.de
 
 # Robots
 export ROBOT=raw3-3
+#export ROBOT=bmw-i3
 #export ROBOT=cob4-2
 
 # Environments
 export ROBOT_ENV=ipa-apartment
+#export ROBOT_ENV=ipa-production-plant
+#export ROBOT_ENV=empty
 #export ROBOT_ENV=ipa-office
 #export ROBOT_ENV=saturn-ingolstadt
+#export ROBOT_ENV=tempodrom_berlin
+#export ROBOT_ENV=IZS-carpark-top
+#export ROBOT_ENV=mulag_hof
 
 # Master URI
 #export ROS_MASTER_URI=http://raw3-3-pc1:11311
