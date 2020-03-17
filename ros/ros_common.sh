0e9b6e1f8adcf13cdaad34bfe90d5f2b7ee6ba77
ipa-srd <Stefan.Doerr@ipa.fraunhofer.de>
2020-03-17 17:09:41 +0100

    ssd

--- ros/ros_common.sh
+++ ros/ros_common.sh
@@ -1,10 +1,7 @@
-#source ~/git/msh_ws/devel/setup.bash
-#source ~/git/baer_ws/devel/setup.bash
-#source ~/git/nav_ws/devel/setup.bash
-#source ~/git/lts_ws/devel/setup.bash
-#source ~/git/afkar_ws/devel/setup.bash
-#source ~/git/wisag_ws/devel/setup.bash
-source ~/git/striml_ws/devel/setup.bash
+#source ~/workspaces/ros_workspaces/nav_ws/devel/setup.bash
+source ~/dev/ros_ws/bmwstr_ws/devel/setup.bash
+#source ~/dev/ros_ws/slam_toolbox_ws/devel/setup.bash
+#source ~/workspaces/ros_workspaces/atf_ws/devel/setup.bash
 
 #set ROS environment variables
 export ROS_EMAIL=srd@ipa.fhg.de
@@ -12,14 +9,16 @@ export ROS_EMAIL=srd@ipa.fhg.de
 # Robots
 #export ROBOT=raw3-3
 #export ROBOT=str_one_sim
-export ROBOT=str_one
+#export ROBOT=str_v2
+export ROBOT=str_v3
 #export ROBOT=bmw-i3
 #export ROBOT=cob4-2
 
 # Environments
 #export ROBOT_ENV=ipa-apartment
+#export ROBOT_ENV=ipa-benchmark
 #export ROBOT_ENV=werk-regensburg-halle-56
-#export ROBOT_ENV=bmw-factory
+export ROBOT_ENV=bmw-factory
 #export ROBOT_ENV=ipa-production-plant
 #export ROBOT_ENV=empty
 #export ROBOT_ENV=ipa-office
@@ -28,7 +27,7 @@ export ROBOT=str_one
 #export ROBOT_ENV=tempodrom_berlin
 #export ROBOT_ENV=IZS-carpark-top
 #export ROBOT_ENV=mulag_hof
-export ROBOT_ENV=cemat_test
+#export ROBOT_ENV=cemat_test
 
 # Master URI
 #export ROS_MASTER_URI=http://raw3-3-pc1:11311
